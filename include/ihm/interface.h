8b34a600966a2593351f035e6efb7b6361bbb55a
LaetitiaNanni <laetitia.nanni@gmail.com>
2013-04-23 22:43:43 +0200

    ajout du fichier inteface.h

---
+++ include/ihm/interface.h
@@ -0,0 +1,13 @@
+#ifndef INTERFACE_H_
+#define INTERFACE_H_
+
+/** panneau latéral **/
+// argent
+// nombre de vagues
+// bouton aide
+// bt mettre le jeu en pause?
+
+/** menu horizontal bas **/
+// bt tour rocket, lazer, mitrallette, hybrid
+
+#endif
